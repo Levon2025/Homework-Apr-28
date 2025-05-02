6ebe9544c663198ea8930f2ac177db0c03ba5b8b
Levon2025 <levonbabajanyan36@gmail.com>
2025-05-02 15:38:04 +0400

    Prime.c

---
+++ Prime.c
@@ -0,0 +1,13 @@
+prime #include "math.h"
+
+int prime_check(int n){
+    if(n < 2){
+        return 0;
+    }
+    for(int i = 2;i*i <= n;i++){
+        if(n % i == 0){
+            return 0;
+        }
+        return 1;
+    }
+}
